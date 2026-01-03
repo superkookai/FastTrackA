27fd7264388d202eade8fee8e07ae824a56d5e97
superkookai <weerawut@live.com>
2026-01-03 09:38:05 +0700

    Initial Commit

---
+++ FastTrackA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastTrackA
+//
+//  Created by Weerawut on 3/1/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastTrackA/FastTrackAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackAApp.swift
+//  FastTrackA
+//
+//  Created by Weerawut on 3/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
